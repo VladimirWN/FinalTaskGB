a0aeba4afd80a348b580650eed3a16261e8b2adf
Vladimir <fok.vov@gmail.com>
2022-07-25 11:00:41 +0300

    method CountCorrectElements

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] MainTask(string[] inputArray)
+{
+    int CountCorrectElements(string[] arr)
+    {
+        int result = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i].Length <= 3) result++;
+        }
+        return result;
+    }
+
+}
