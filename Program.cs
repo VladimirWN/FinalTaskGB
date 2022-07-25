104e87562a411f83d69a7f131baf1588cc432424
Vladimir <fok.vov@gmail.com>
2022-07-25 11:06:07 +0300

    method MainTask

--- Program.cs
+++ Program.cs
@@ -10,4 +10,18 @@
         return result;
     }
 
+
+    int count = CountCorrectElements(inputArray);
+    string[] newArray = new string[count];
+    int index = 0;
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            newArray[index] = inputArray[i];
+            index++;
+        }
+    }
+    return newArray;
 }
